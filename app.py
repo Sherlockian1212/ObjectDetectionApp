8b223b0d6d3cb03ccde29f08529d334351f2e1cb
Ín <51265048+Sherlockian1212@users.noreply.github.com>
2024-10-28 22:56:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,10 +27,29 @@ def detect():
     image_bytes = base64.b64decode(image_data.split(",")[1])
     image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
 
+    detected_objects = []
+    class_names = [
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", 
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", 
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", 
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", 
+    "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", 
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", 
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", 
+    "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", 
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", 
+    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+    ]
     # Phát hiện vật thể
     results = model(image)
-    detected_objects = ["shirt"]
-    
+    for result in results:
+        if result:
+            class_ids = result.boxes.cls
+            for class_id in class_ids:
+                class_name = class_names[int(class_id)]
+                print(class_name)
+                detected_objects.append(class_name)
+            
     if detected_objects:
         speak_results(detected_objects)
 
