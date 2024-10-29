26d47f56d1f040464965d666715bd0c149b61f68
Ín <51265048+Sherlockian1212@users.noreply.github.com>
2024-10-29 08:11:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -62,5 +62,5 @@ def speak_results(objects):
     playsound("result.mp3")
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 4000))
     app.run(host="0.0.0.0", port=port, debug=True)
